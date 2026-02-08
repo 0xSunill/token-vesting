3d148a251c3f283f385321ff3e1799b698c0015d
0xSunill <sunday7637@gmail.com>
2026-02-08 05:52:11 +0000

    all instruction for vesting

--- anchor/programs/vault/src/lib.rs
+++ anchor/programs/vault/src/lib.rs
@@ -1,5 +1,6 @@
 use anchor_lang::prelude::*;
 use anchor_lang::system_program::{transfer, Transfer};
+use anchor_spl::token_interface::{Mint, TokenAccount, TokenInterface};
 
 #[cfg(test)]
 mod tests;
@@ -10,6 +11,52 @@ declare_id!("H2CpMZemyu7b1R5AVPcYsqAdYaE1oJ8D2YwbrSGdZaLT");
 pub mod vesting {
     use super::*;
 
+    pub fn create_vesting_account(
+        ctx: Context<CreateVestingAccount>,
+        company_name: String,
+        amount: u64,
+    ) -> Result<()> {
         Ok(())
     }
+}
+
+#[derive(Accounts)]
+#[instruction(company_name:String)]
+pub struct CreateVestingAccount<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + VestingAccount::INIT_SPACE,
+        seeds = [company_name.as_ref()],
+        bump,
+    )]
+    pub vesting_account: Account<'info, VestingAccount>,
+
+    pub mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        init,
+        token::mint = mint,
+        token::authority = treasury_token_account,
+        payer = signer,
+        seeds = [b"vesting_treasury", company_name.as_bytes()],
+        bump
+    )]
+    pub treasury_token_account: InterfaceAccount<'info, TokenAccount>,
+    pub token_program: Program<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct VestingAccount {
+    pub owner: Pubkey,
+    pub mint: Pubkey,
+    pub treasury_account: Pubkey,
+    #[max_len(32)]
+    pub company_name: String,
+    pub treasury_bump: u8,
+    pub bump: u8,
 }
